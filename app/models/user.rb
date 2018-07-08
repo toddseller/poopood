7fdebd6b2edb70927064585e0edad3778cc74242
toddseller <me@toddseller.com>
2018-07-08 09:15:28 -0700

    Add indices to User table and token to User model git status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+  has_secure_token :confirmation_token
+  
 end
--- db/migrate/20180708151014_create_users.rb
+++ db/migrate/20180708151014_create_users.rb
@@ -2,13 +2,16 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users, id: :uuid do |t|
       t.string    :confirmation_token
-      t.datetime  :confirmed_at
+      t.boolean   :confirmed,               default: false
 
-      t.string    :email,                 null: false
-      t.string    :first_name,            default: ''
-      t.string    :last_name,             default: ''
+      t.string    :email,                   null: false
+      t.string    :first_name,              default: ''
+      t.string    :last_name,               default: ''
 
-      t.timestamps      null: false
+      t.timestamps                          null: false
     end
+
+    add_index :users, :confirmation_token,  unique: true
+    add_index :users, :email,               unique: true
   end
 end
