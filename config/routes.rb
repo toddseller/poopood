c531f4eeb8d6a272d57d71aa98de37402982fa2d
toddseller <me@toddseller.com>
2018-07-09 12:47:20 -0700

    Update routes again

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/index'
-  
-  get '/confirmation_email/:token', to: "users#confirmation_email", as: 'confirmation_email'
-  get '/unsubscribe_email/:token', to: "users#unsubscribe_email", as: 'unsubscribe_email'
+
+  get '/confirmation_email', to: "users#confirmation_email", as: 'confirmation_email'
+  get '/unsubscribe_email', to: "users#unsubscribe_email", as: 'unsubscribe_email'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
