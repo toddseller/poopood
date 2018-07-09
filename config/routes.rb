7b4931618526be74609202b56db3fe879b192ff0
toddseller <me@toddseller.com>
2018-07-09 12:36:32 -0700

    Restore original routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/index'
-
-  resources :users do
-    member do
-      get :confirmation_email
-      get :unsubscribe_email
-    end
-  end
+  
+  get '/confirmation_email/:token', to: "users#confirmation_email", as: 'confirmation_email'
+  get '/unsubscribe_email/:token', to: "users#unsubscribe_email", as: 'unsubscribe_email'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
